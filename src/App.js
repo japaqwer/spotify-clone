6bf8961d4276629519ffc7fb2fd2fb28680d2959
MacBook_Pro <macbook_pro@MacBook-Pro-MacBook-Pro.local>
2023-02-08 20:47:12 +0600

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Router from './router/router';
+
+//Styles
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useTitle.jsx
@@ -0,0 +1,10 @@
+import { useCallback } from "react"
+
+export const useTitle = () =>{
+    const rename = useCallback((value) =>{
+        document.title = value 
+    },[])
+    return{
+        rename
+    }
+}
